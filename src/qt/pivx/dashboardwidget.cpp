70ceea2996f460f2c9506305f3bea99fc71ed3a3
Altecoin Crypto <87372685+altecoin-altc@users.noreply.github.com>
2024-03-27 09:26:16 +0800

    color

--- src/qt/pivx/dashboardwidget.cpp
+++ src/qt/pivx/dashboardwidget.cpp
@@ -674,7 +674,7 @@ void DashboardWidget::onChartRefreshed()
     // init sets
     set0 = new QBarSet(CURRENCY_UNIT.c_str());
 	set1 = new QBarSet("MN_" + QString(CURRENCY_UNIT.c_str()));
-    set0->setColor(QColor(14, 178, 21));
+    set0->setColor(QColor(14, 178, 216));
 	set1->setColor(QColor(0, 70, 119));
 
     if (!series) {
